342a6eb8eaa8cec895f8772a2b9c33e160328c41
Brijal <brijalkansara091@gmail.com>
2022-03-31 22:49:39 +0530

    Tutorial 4, Adding Bootstrap, crating TestUtils Project Start

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# react-learning
+Hello There! I am learning React at this point. and i have found the best code with harry tutorials for learning react and i am following itl 
--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,34 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
-let name = "Brijal Kansara";
-let title = <h1>Hello {name}</h1>
-
-let disc = <b>She is an MCA student and she is really good at her woek. Proud of you </b>
-let para = <p>I am just a PAragraph. I am enjoying my work </p>
 function App() {
   return (
     <>
-    <div className="nav-bar">
-      <li>Home</li>
-      <li>About</li>
-      <li>Login</li>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">TestUtils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
-
-    {/* do not use quotes near braces here not that thing, this is js variable. */}
-    {/* Usign Javascript variables that containing JSX and strings variables. */}
-    {title}
-    {disc}
-    {para }
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quisquam eius qui laboriosam ad tempora sit voluptatum quae vero dolorem. Nesciunt natus officia eum inventore quos nostrum assumenda aperiam velit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo excepturi sed, inventore corporis velit nihil unde, deserunt autem natus, sapiente commodi ea nemo itaque aut cumque voluptates culpa expedita nisi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sapiente quo alias vero esse maiores necessitatibus deserunt accusamus voluptate culpa assumenda rem, minus tenetur fugiat, quod ipsum, nobis voluptatibus inventore.
-    
-    Ex magnam officia, aliquam culpa provident assumenda corporis quas qui repudiandae accusantium repellat adipisci modi laboriosam veritatis itaque at cupiditate quia consectetur placeat in eaque soluta quam iusto facere. Id?
-    Fuga repudiandae, eveniet minus dolore perspiciatis cum? Non ipsa, qui, odit corporis omnis expedita magni exercitationem beatae assumenda, cupiditate quis a nostrum! Voluptatibus, iure architecto adipisci inventore soluta vero cumque!
-    
-       </p>
-    <div className="main-app">
-     Lovely
-   </div></>
+  </div>
+</nav>
+   </>
   );
 }
 
