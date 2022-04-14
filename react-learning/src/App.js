8116076fb21860f8688cc62c19092d9f0c4243ee
Brijal <brijalkansara091@gmail.com>
2022-04-14 10:38:20 +0530

    text color changing on Darkmode changing and light mode changing.

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -31,7 +31,8 @@ function App() {
     if(mode === 'light'){
       setMode('dark');
       $('body').css('background-color', 'rgb(105, 105, 115)');
-      $("#main-container-body").attr('style', "Filter: invert(100%);")
+      $('body').css('color', 'white');
+      // $("#main-container-body").attr('style', "Filter: invert(100%);")
       setswitchMode("LightMode");
       showAlert("Dark Mode Enabled SusessFully", "success");
       // We are using Ternary operator of javascript so i have commented it
@@ -40,7 +41,8 @@ function App() {
     else{
       setMode('light');
       $('body').css('background-color', 'white');
-      $("#main-container-body").attr('style', "Filter: invert(0%);")
+      $('body').css('color', 'black');
+      // $("#main-container-body").attr('style', "Filter: invert(0%);")
       setswitchMode("Darkmode");
       showAlert("Light Mode Enabled SusessFully", "success");
       // We are using Ternary operator of javascript so i have commented it
