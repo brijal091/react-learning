33fcc4d3a255b3ae6b1520ad353ff1e621a365df
Brijal <brijalkansara091@gmail.com>
2022-04-17 15:02:08 +0530

    Newsapp structure, Navbar, News component and News Item Component Added

--- NewsApp/newsapp/src/App.js
+++ NewsApp/newsapp/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
-  c = 'john'
   render() {
     return (
       <div>
-        This is my First class based component. Hi {this.c}
+        <Navbar />
+        <News />
       </div>
     )
   }
