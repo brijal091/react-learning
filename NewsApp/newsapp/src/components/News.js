7eb05b999dc19c8eacb408f080a294703ef526df
Brijal <brijalkansara091@gmail.com>
2022-04-18 21:58:47 +0530

    Tutorial 24 completed. Have Added Cards to and json of data.

--- NewsApp/newsapp/src/components/News.js
+++ NewsApp/newsapp/src/components/News.js
@@ -5,13 +5,29 @@ export class News extends Component {
   render() {
     return (
       <div>
-        I am News Component
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+        <div className="container my-3">
+          <h2 className='my-5'>News Monkey | Top HeadLines </h2>
+          <div className="row">
+              <div className="col-md-4 my-3">
+                <NewsItem title="My Title" description="My Description Testing for components"/>
+              </div>
+              <div className="col-md-4 my-3">
+                <NewsItem title="My Title" description="My Description Testing for components"/>
+              </div>
+              <div className="col-md-4 my-3">
+                <NewsItem title="My Title" description="My Description Testing for components"/>
+              </div>
+              <div className="col-md-4 my-3">
+                <NewsItem title="My Title" description="My Description Testing for components"/>
+              </div>
+              <div className="col-md-4 my-3">
+                <NewsItem title="My Title" description="My Description Testing for components"/>
+              </div>
+              <div className="col-md-4 my-3">
+                <NewsItem title="My Title" description="My Description Testing for components"/>
+              </div>
+            </div>
+        </div>
       </div>
     )
   }
--- NewsApp/newsapp/src/components/NewsItem.js
+++ NewsApp/newsapp/src/components/NewsItem.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    // we need to use props in class based components. 
+    let {title, description} = this.props;
     return (
       <div>
-        I am NewsItem Component
+        <div className="card" style={{width: "18rem"}}>
+            <img src="https://static01.nyt.com/images/2022/04/15/nyregion/00virus_family_explainer1/00virus_family_explainer1-facebookJumbo.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              {/* Using Props  */}
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a href="/" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
       </div>
     )
   }
